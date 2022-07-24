1408c57530549ab7cd926dd03f5a4996073ec234
Oscar Monroy <oscarmonroy657@gmail.com>
2022-07-24 07:57:52 -0600

    The EntryList and EntryItem components have been added and their styles are ready.

---
+++ src/modules/daybook/components/EntryItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="entry-container mb-3 pointer p-2">
+    <div class="entry-title d-flex">
+      <span class="text-success fs-5 fw-bold">15</span>
+      <span class="mx-1 fs-5">July</span>
+      <span class="mx-2 fw-light">2022</span>
+    </div>
+    <div class="entry-description">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In maiores modi
+        voluptates fuga eveniet, nemo mollitia obcaecati, odio illum, provident
+        aliquid earum iste repellat possimus omnis autem suscipit voluptatum
+        sequi? Dolorum, error exercitationem! Veritatis dolores inventore, cum
+        magnam unde quisquam cumque atque, minus veniam laboriosam iusto ipsum
+        consequuntur reprehenderit ab assumenda repellendus. Soluta eum est
+        eaque temporibus ea. Assumenda, molestiae!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.entry-container {
+  border-bottom: 1px solid #2c3e50;
+  transition: 0.2s ease-in;
+
+  &:hover {
+    background-color: lighten($color: grey, $amount: 45);
+    transition: 0.2s all ease-in;
+  }
+}
+.entry-description {
+  font-size: 12px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
   </div>
-  <button class="btn btn-primary">Primary</button>
-  <button class="btn btn-secondary">Secondary</button>
-  <button class="btn btn-success">Success</button>
+  <button @click="goToDayBook" class="btn btn-primary">Primary</button>
+  <button @click="goToDayBook" class="btn btn-secondary">Secondary</button>
+  <button @click="goToDayBook" class="btn btn-success">Success</button>
 </template>
 
 <script>
 export default {
   name: "HomeView",
   components: {},
+  methods: {
+    goToDayBook() {
+      this.$router.push({ name: "daybook" });
+    },
+  },
 };
 </script>
