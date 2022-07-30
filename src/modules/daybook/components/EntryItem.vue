23ab067cd737da1f22fb55a9f8c67966f8e6ac01
Oscar Monroy <oscarmonroy657@gmail.com>
2022-07-30 17:45:00 -0600

    Display the entries from the store is working.}

--- src/modules/daybook/components/EntryItem.vue
+++ src/modules/daybook/components/EntryItem.vue
@@ -1,29 +1,54 @@
 <template>
   <div
     class="entry-container mb-3 pointer p-2"
-    @click="$router.push({ name: 'entry', params: { id: 10 } })"
+    @click="$router.push({ name: 'entry', params: { id: entry.id } })"
   >
     <div class="entry-title d-flex">
-      <span class="text-success fs-5 fw-bold">15</span>
-      <span class="mx-1 fs-5">July</span>
-      <span class="mx-2 fw-light">2022</span>
+      <span class="text-success fs-5 fw-bold">{{ entryDay }}</span>
+      <span class="mx-1 fs-5">{{ entryMonth }}</span>
+      <span class="mx-2 fw-light">{{ entryYear }}</span>
     </div>
     <div class="entry-description">
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. In maiores modi
-        voluptates fuga eveniet, nemo mollitia obcaecati, odio illum, provident
-        aliquid earum iste repellat possimus omnis autem suscipit voluptatum
-        sequi? Dolorum, error exercitationem! Veritatis dolores inventore, cum
-        magnam unde quisquam cumque atque, minus veniam laboriosam iusto ipsum
-        consequuntur reprehenderit ab assumenda repellendus. Soluta eum est
-        eaque temporibus ea. Assumenda, molestiae!
+        {{ shortText }}
       </p>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    entry: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    shortText() {
+      return this.entry.text.length > 100
+        ? this.entry.text.substr(0, 100) + "..."
+        : this.entry.text;
+    },
+    entryDay() {
+      const day = new Date(this.entry.date).getDate();
+      return day;
+    },
+    entryMonth() {
+      const moth = new Date(this.entry.date).toLocaleString("default", {
+        month: "long",
+      });
+      return moth;
+    },
+    entryYear() {
+      const year = new Date(this.entry.date).toLocaleString("default", {
+        year: "numeric",
+        weekday: "long",
+      });
+      return year;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -38,5 +63,6 @@ export default {};
 }
 .entry-description {
   font-size: 12px;
+  text-align: left;
 }
 </style>
--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,20 +1,41 @@
 <template>
   <div class="entry-list-container">
     <div class="px-2 pt-2">
-      <input type="text" class="form-control" placeholder="Search entry" />
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Search entry"
+        v-model="term"
+      />
     </div>
     <div class="entry-scrollarea">
-      <EntryItem v-for="item in 100" :key="item" />
+      <EntryItem
+        v-for="entry in entriesByTerm"
+        :key="entry.id"
+        :entry="entry"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import { defineAsyncComponent } from "vue";
+import { mapGetters } from "vuex";
 
 export default {
   components: {
     EntryItem: defineAsyncComponent(() => import("./EntryItem.vue")),
+  },
+  computed: {
+    ...mapGetters("journal", ["getEntriesByTerm"]),
+    entriesByTerm() {
+      return this.getEntriesByTerm(this.term);
+    },
+  },
+  data() {
+    return {
+      term: "",
+    };
   },
 };
 </script>
