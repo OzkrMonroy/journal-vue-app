87ca7a14dd1513a56e8a11b769cfd989d6826a64
Oscar Monroy <oscarmonroy657@gmail.com>
2022-07-31 07:49:17 -0600

    The reload option when the url change is ready, also I added a validation to render the template if the entry exists.

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,34 +1,36 @@
 <template>
-  <div class="entry-title d-flex justify-content-between p-2">
-    <div>
-      <span class="text-success fs-3 fw-bold">{{ day }}</span>
-      <span class="mx-1 fs-3">{{ month }}</span>
-      <span class="mx-2 fs-4 fw-light">{{ year }}</span>
+  <template v-if="entry">
+    <div class="entry-title d-flex justify-content-between p-2">
+      <div>
+        <span class="text-success fs-3 fw-bold">{{ day }}</span>
+        <span class="mx-1 fs-3">{{ month }}</span>
+        <span class="mx-2 fs-4 fw-light">{{ year }}</span>
+      </div>
+      <div>
+        <button class="btn btn-danger mx-2">
+          Delete
+          <i class="fa fa-trash-alt"></i>
+        </button>
+        <button class="btn btn-primary">
+          Upload photo
+          <i class="fa fa-upload"></i>
+        </button>
+      </div>
     </div>
-    <div>
-      <button class="btn btn-danger mx-2">
-        Delete
-        <i class="fa fa-trash-alt"></i>
-      </button>
-      <button class="btn btn-primary">
-        Upload photo
-        <i class="fa fa-upload"></i>
-      </button>
+    <hr />
+    <div class="d-flex flex-column px-3 h-75">
+      <textarea
+        placeholder="what happened today?"
+        v-model="entry.text"
+      ></textarea>
     </div>
-  </div>
-  <hr />
-  <div class="d-flex flex-column px-3 h-75">
-    <textarea
-      placeholder="what happened today?"
-      v-model="entry.text"
-    ></textarea>
-  </div>
+    <img
+      src="https://estaticos.muyinteresante.es/uploads/images/gallery/5e723bc25cafe8a3f89605a5/flores_0.jpg"
+      alt="entry-picture"
+      class="img-thumbnail"
+    />
+  </template>
   <FabButton icon="fa-save" />
-  <img
-    src="https://estaticos.muyinteresante.es/uploads/images/gallery/5e723bc25cafe8a3f89605a5/flores_0.jpg"
-    alt="entry-picture"
-    class="img-thumbnail"
-  />
 </template>
 
 <script>
@@ -51,7 +53,7 @@ export default {
   methods: {
     getEntry() {
       const entry = this.getEntryById(this.id);
-      if (!entry) this.$router.push({ name: "no-entry" });
+      if (!entry) return this.$router.push({ name: "no-entry" });
 
       this.entry = entry;
     },
@@ -75,6 +77,11 @@ export default {
   },
   created() {
     this.getEntry();
+  },
+  watch: {
+    id() {
+      this.getEntry();
+    },
   },
 };
 </script>
