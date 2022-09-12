900b02500413bdb193a4dc1657eec40952bc196e
Oscar Monroy <oscarmonroy657@gmail.com>
2022-09-11 20:19:43 -0600

    An interceptor has been added to use the user token id

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -4,4 +4,12 @@ const journalApi = axios.create({
   baseURL: "https://sample-apps-43c93-default-rtdb.firebaseio.com",
 });
 
+journalApi.interceptors.request.use((config) => {
+  config.params = {
+    auth: localStorage.getItem("idToken"),
+  };
+
+  return config;
+});
+
 export default journalApi;
